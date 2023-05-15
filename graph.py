d1b0fc33e2a26805048b3c1988235d717666b6b7
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-05-15 19:40:41 +0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -3,7 +3,7 @@ import re
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv("score.csv", low_memory=False)
+df = pd.read_csv("py4ai-score.csv", low_memory=False)
 st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
 
 def sohs():
