c5fbb4b5e6691bdb1a23fdcc40910425956ab956
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-05-16 22:13:54 +0700

    Update and rename graph.py to kmeans.py

--- graph.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-df = pd.read_csv("py4ai-score.csv", low_memory=False)
-st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
---
+++ kmeans.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+import numpy as np 
+import plotly.express as px
+df = pd.read_csv("py4ai-score.csv", low_memory=False)
+
+X = df[['S6', 'S10', 'GPA']].to_numpy()
+kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
+print(kmeans.labels_)
+x = df['S6'].values
+y = df['S10'].values
+z = df['GPA'].values
+
+fig = px.scatter_3d(x=x, y=y, z=z, color=kmeans.labels_)
+fig.show()
+
+print(df[kmeans.labels_==0])
+print(df[kmeans.labels_==1])
+print(df[kmeans.labels_==2])
