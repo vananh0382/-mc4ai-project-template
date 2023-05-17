a17f3b35a4d4627fd3736866781dba5161f79c0f
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-05-17 19:47:54 +0700

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -23,5 +23,7 @@ z = df['GPA'].values
 fig = px.scatter_3d(x=x, y=y, z=z, color=kmeans.labels_)
 st.plotly_chart(fig, use_container_width=False)
 for i in range(num):
-  st.write(df[kmeans.labels_==i].drop(columns=['GENDER','CLASS','PYTHON-CLASS','S1','S2','S3','S4','S5','S6','S7','S8','S9','S10','BONUS','REG-MC4AI']))
+  dfi=df[kmeans.labels_==i]
+  st.write('Nhóm' ,i, 'GPA cao nhất là',dfi['GPA'].values.max(),'thấp nhất là',dfi['GPA'].values.min(),' trung bình',dfi['GPA'].values.sum())
+  st.write(dfi.drop(columns=['GENDER','CLASS','PYTHON-CLASS','S1','S2','S3','S4','S5','S6','S7','S8','S9','S10','BONUS','REG-MC4AI']))
 
