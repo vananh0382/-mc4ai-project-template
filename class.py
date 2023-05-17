ed6653afe44a67822f22173f724a0a9f78264340
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-05-17 22:46:29 +0700

    Update class.py

--- class.py
+++ class.py
@@ -1,4 +1,5 @@
 from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np 
 import plotly.express as px
@@ -26,12 +27,23 @@ def mhpf():
     else:
       y.append(0)
   y = np.array(y)
-  plt.scatter(X[y==0,0], X[y==0,1])
-  plt.scatter(X[y==1,0], X[y==1,1])
-  plt.legend(['S6', 'S-AVG'])
-  plt.xlabel('S-AVG')
-  plt.ylabel('S6')
-  plt.show()
-  st.pyplot(fig=None)
+
+
+ model = LogisticRegression()
+ model.fit(X, y)
+ weights = model.coef_[0]
+ bias = model.intercept_[0]
+ weights, bias
+ w1, w2 = weights[0], weights[1]
+ plt.scatter(X[y==0,0], X[y==0,1])
+ plt.scatter(X[y==1,0], X[y==1,1])
+ plt.legend(['S6', 'S-AVG', 'Decision Boundary'])
+ plt.xlabel('S-AVG')
+ plt.ylabel('S6')
+ x1 = np.linspace(0,10,1000)
+ x2 = -(w1*x1+bias)/w2
+ plt.plot(x1,x2)
+ st.pyplot(fig=None)
+ 
 
 mhpf()
