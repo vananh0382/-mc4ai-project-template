a81d4a1bd71f3b8de7b67c3c9500871401890a58
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-05-17 21:41:20 +0700

    Update giaodien.py

--- giaodien.py
+++ giaodien.py
@@ -5,6 +5,12 @@ import numpy as np
 import plotly.express as px
 import streamlit as st
 
+ df = pd.read_csv("py4ai-score.csv", low_memory=False)
+ df['BONUS'].fillna(0, inplace = True)
+ for i in range(1, 11):
+   df[f"S{i}"].fillna(0, inplace = True)
+ df['REG-MC4AI'].fillna("N", inplace = True)
+
 option = st.selectbox('Chọn mục', ('Danh sách', 'Biểu đồ', 'Phân nhóm', 'Phân loại'))
 if option =='Phân nhóm':
   kmeans.kmeans()
