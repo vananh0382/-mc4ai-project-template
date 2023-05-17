02365e2a9000f280c0c6ee4b21083d7a0f9b98ed
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-05-17 21:30:08 +0700

    Create giaodien.py

---
+++ giaodien.py
@@ -0,0 +1,10 @@
+import kmeans
+from sklearn.cluster import KMeans
+import pandas as pd
+import numpy as np 
+import plotly.express as px
+import streamlit as st
+
+option = st.selectbox('Chọn mục', ('Danh sách', 'Biểu đồ', 'Phân nhóm', 'Phân loại'))
+if option =='Phân nhóm':
+  kmeans.kmeans()
