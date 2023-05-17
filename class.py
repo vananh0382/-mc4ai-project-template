85e135c782d5931012a77cb91143711207022e99
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-05-17 22:21:09 +0700

    Update class.py

--- class.py
+++ class.py
@@ -4,6 +4,13 @@ import numpy as np
 import plotly.express as px
 import streamlit as st
 import matplotlib.pyplot as plt
+
+df = pd.read_csv("py4ai-score.csv", low_memory=False)
+df['BONUS'].fillna(0, inplace = True)
+for i in range(1, 11):
+ df[f"S{i}"].fillna(0, inplace = True)
+df['REG-MC4AI'].fillna("N", inplace = True)
+
 avg = []
 for i in df.index:
   r = df.loc[i][4:13]
@@ -12,7 +19,6 @@ df['S-AVG'] = avg
 
 def mhpf():
   X = df[['S6','S-AVG']].values.copy()
-
   y = []
   for i in range(len(df[['NAME']])):
     if X[i][0] >= 6 and X[i][1] >= 6:
