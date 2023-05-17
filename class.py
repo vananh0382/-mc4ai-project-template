a48289e26d86866d8cda2c5c9351bbae1c1dee91
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-05-17 22:14:36 +0700

    Create class.py

---
+++ class.py
@@ -0,0 +1,29 @@
+from sklearn.cluster import KMeans
+import pandas as pd
+import numpy as np 
+import plotly.express as px
+import streamlit as st
+import matplotlib.pyplot as plt
+avg = []
+for i in df.index:
+  r = df.loc[i][4:13]
+  avg.append(r.mean())
+df['S-AVG'] = avg
+
+def mhpf():
+  X = df[['S6','S-AVG']].values.copy()
+
+  y = []
+  for i in range(len(df[['NAME']])):
+    if X[i][0] >= 6 and X[i][1] >= 6:
+      y.append(1)
+    else:
+      y.append(0)
+  y = np.array(y)
+  plt.scatter(X[y==0,0], X[y==0,1])
+  plt.scatter(X[y==1,0], X[y==1,1])
+  plt.legend(['S6', 'S-AVG'])
+  plt.xlabel('S-AVG')
+  plt.ylabel('S6')
+  plt.show()
+  st.pyplot(fig)
