74607b8eee51d203d1286740dd3f97ed5c27af04
vananh0382 <125394307+vananh0382@users.noreply.github.com>
2023-05-18 13:24:02 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import kmeans
+from sklearn.cluster import KMeans
+import pandas as pd
+import numpy as np 
+import plotly.express as px
+import streamlit as st
+
+from class import class
+from graph import graph
+from kmeans import kmeans
+
+df = pd.read_csv("score.csv", low_memory=False)
+st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
+
+tab1, tab2, tab3, tab4 = st.tabs(['Danh sách', 'Biểu đồ', 'Phân nhóm', 'Phân loại'])
+with tab1:
+  
+with tab2:
+  graph()
+with tab3:
+  kmeans.kmeans()
+with tab4:
+  class()
