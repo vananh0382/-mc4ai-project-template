b0e384675051511fbf987f2cd2d6c84aea55a1cf
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-05-18 19:16:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ df = pd.read_csv("score.csv", low_memory=False)
 st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
 
 tab1, tab2, tab3, tab4 = st.tabs(['Danh sách', 'Biểu đồ', 'Phân nhóm', 'Phân loại'])
-with tab1:
+# with tab1:
   
 with tab2:
   graph.graph()
 with tab3:
   kmeans.kmeans()
-with tab4:
+# with tab4:
 #   class()
