6ed20f0ac39003a20b5b20cbfaa44c603e8d432a
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-05-18 20:06:53 +0700

    Update classi.py

--- classi.py
+++ classi.py
@@ -29,21 +29,21 @@ def classi():
         else:
           y.append(0)
       y = np.array(y)
-     model = LogisticRegression()
-     model.fit(X, y)
-     weights = model.coef_[0]
-     bias = model.intercept_[0]
-     w1, w2 = weights[0], weights[1]
+      model = LogisticRegression()
+      model.fit(X, y)
+      weights = model.coef_[0]
+      bias = model.intercept_[0]
+      w1, w2 = weights[0], weights[1]
 
-     plt.scatter(X[y==0,0], X[y==0,1])
-     plt.scatter(X[y==1,0], X[y==1,1])
-     plt.legend(['S6', 'S-AVG', 'Decision Boundary'])
-     plt.xlabel('S-AVG')
-     plt.ylabel('S6')
-     x1 = np.linspace(0, 10, 1000)
-     x2 = -(w1*x1+bias)/w2
-     plt.plot(x1,x2)
-     st.pyplot(fig=None)
+      plt.scatter(X[y==0,0], X[y==0,1])
+      plt.scatter(X[y==1,0], X[y==1,1])
+      plt.legend(['S6', 'S-AVG', 'Decision Boundary'])
+      plt.xlabel('S-AVG')
+      plt.ylabel('S6')
+      x1 = np.linspace(0, 10, 1000)
+      x2 = -(w1*x1+bias)/w2
+      plt.plot(x1,x2)
+      st.pyplot(fig=None)
     mhpf()
 
     def mgpf():
